ecca5e95e7657c8b274899288252a5b98369157d
Roxana <rocevedo00@gmail.com>
2022-02-09 01:10:33 -0300

    last commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -61,11 +61,10 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  If (x==y) {
+  if (x===y) {
   return true; 
-}  else {
-return false;
-}
+} else {
+  return false;}
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -175,15 +174,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0); {
-  return "es positivo";
- } 
- else if(numero < 0);{
-  return "es negarivo"
- }
- else(numero == 0); {
- return false;
- }
+  if (numero > 0) return "es positivo";
+  if (numero < 0) return "es negarivo";
+  else (numero === 0); return false;
  }
 
 function agregarSimboloExclamacion(str) {
@@ -205,20 +198,20 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
- return "hola " + "roxana"  + "!"
+ return "hola " + (nombre )  + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return alto* ancho;
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  lado * 4;
+  return lado * 4;
 }
 
 
@@ -243,10 +236,10 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí 
-  if (letra == a) || (letra == e) || (letra == i) || (letra == o) || (letra == u) {
+  if (letra == a || letra == e || letra == i || letra == o || letra == u); {
     return "es vocal"
   }
-  if else (letra.length > 1) {
+  if (letra.length > 1); {
     return "dato incorrecto"
   }
  return "dato incorrecto"
