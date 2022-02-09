350e46c021831afe86754c69445912cb5a918b09
Roxana <rocevedo00@gmail.com>
2022-02-09 02:43:40 -0300

    last

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -242,7 +242,7 @@ function esVocal(letra){
   if (letra.length > 1) {
     return "Dato incorrecto";
   } 
-  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"); {
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
     return "Es vocal"; } 
   else {
  return "Dato incorrecto" }
