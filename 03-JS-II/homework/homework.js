c5a896b5988b64b23bbfb158c5b75abe35f7d6cf
Roxana <rocevedo00@gmail.com>
2022-02-11 13:18:50 -0300

    is it right

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -133,18 +133,15 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
- if (num1 > num2 && num1 > num3 && num1 > 0) {
-   return "Número 1 es mayor y positivo"; 
- }
-  if (num1 < 0 || num2 < 0 || num3 < 0) {
-   return "Hay negativos"; 
- }
-  if (num1 === 0 || num2 === 0 || num3 === 0) {
-   return "Error"; 
- } 
+ if (num1 === 0 || num2 === 0 || num3 === 0) {
+   return "Error"; }
+ if (num1 < 0 || num2 < 0 || num3 < 0) {
+   return "Hay negativos"; }
  if (num3 > num2 || num3 > num1) {
-   return num3 + 1;
- } else { return false }
+   return num3 + 1; }
+ if (num1 > num2 && num1 > num3 && num1 > 0) {
+   return "Número 1 es mayor y positivo"; } 
+ else { return false }
 }
 
 function esPrimo(numero) {
@@ -153,9 +150,9 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- if (numero % 1 === 0 && numero % numero === 0) {
-   return true;
- } else { return false; } 
+ if (numero % 1 === 0 || numero % numero === 0) {
+   return true} 
+ else { return false } 
 }
 
 function esVerdadero(valor){
@@ -163,8 +160,8 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 if (valor >= 0) {
-  return "Soy verdadedo"
-} else {return "Soy falso"}
+  return "Soy verdadedo"; 
+} else {return "Soy falso"; }
 }
 
 function tablaDelSeis(){
@@ -172,7 +169,7 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   for (var i = 1; i <= 60; i + 6) {
-    return i; 
+    return i;
   }
 }
 
