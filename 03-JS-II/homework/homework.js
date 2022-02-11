27730080e403e211aaba2f48c4c91fec62a26d2e
Roxana <rocevedo00@gmail.com>
2022-02-11 13:36:10 -0300

    last

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -134,13 +134,13 @@ function operadoresLogicos(num1, num2, num3) {
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
  if (num1 === 0 || num2 === 0 || num3 === 0) {
-   return "Error"; }
+   return "Error"; } 
  if (num1 < 0 || num2 < 0 || num3 < 0) {
    return "Hay negativos"; }
+ if (num1 > num2 && num1 > num3 && num1 > 0) {
+   return "Número 1 es mayor y positivo"; }
  if (num3 > num2 || num3 > num1) {
    return num3 + 1; }
- if (num1 > num2 && num1 > num3 && num1 > 0) {
-   return "Número 1 es mayor y positivo"; } 
  else { return false }
 }
 
@@ -151,8 +151,9 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
  if (numero % 1 === 0 || numero % numero === 0) {
-   return true} 
- else { return false } 
+   return true}
+ else (numero === 0 || numero === 1); { 
+   return false }
 }
 
 function esVerdadero(valor){
@@ -161,14 +162,15 @@ function esVerdadero(valor){
   //Escribe tu código aquí
 if (valor >= 0) {
   return "Soy verdadedo"; 
-} else {return "Soy falso"; }
+} else (valor < 0); {
+  return "Soy falso"};
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for (var i = 1; i <= 60; i + 6) {
+  for (var i = 1; i <= 60; i++ ) {
     return i;
   }
 }
