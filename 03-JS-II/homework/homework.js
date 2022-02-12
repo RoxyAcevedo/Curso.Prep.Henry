19e3548bf390ca397dfaee29ba38164bb88d48c8
Roxana <rocevedo00@gmail.com>
2022-02-12 15:40:02 -0300

    numeroprimo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -150,14 +150,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero === 0 || numero === 1){ 
+    return false; }
+  for (var i = 2; i > 2 ; i++) {
+    if (numero % i === 0) {
+      return false}  
   if (numero % 1 === 0 || numero % numero === 0){
     return true; }
-  if (numero === 0 || numero === 1){ 
-    return false ; }
-  for (i > 2 ; i >numero; i++) {
-    if (numero % i === 0) {return false; 
-  }
-}
 }
 
 function esVerdadero(valor){
