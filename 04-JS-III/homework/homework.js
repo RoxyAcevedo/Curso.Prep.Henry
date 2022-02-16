237dd29648db0dc19459ee5c89058e967a5b060c
Roxana <rocevedo00@gmail.com>
2022-02-16 16:27:22 -0300

    t

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -230,7 +230,7 @@ function breakStatement(numero) {
         i++;
         return "Se interrumpio la ejecucion";
      }
-}
+} 
 
 
 function continueStatement(numero) {
@@ -240,7 +240,7 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  
+
 }
 
 
