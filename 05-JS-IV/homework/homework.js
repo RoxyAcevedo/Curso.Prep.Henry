65c3524a33e812876ae1a08812275d7015d8bd56
Roxana <rocevedo00@gmail.com>
2022-02-17 22:30:16 -0300

    seven

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,7 +21,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto.property = null;
+  objeto[property] = null;
   return objeto;
 }
 
@@ -30,13 +30,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+var multiplicar = objetoMisterioso["numeroMisterioso"] * 5; 
+  return multiplicar;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -44,13 +46,20 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto["unaPropiedad"] 
+    return objeto; 
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var nuevoUsuario = {
+    nombre: nombre, 
+    email: email, 
+    password: password,
+  } 
+  return nuevoUsuario;
 }
 
 function tieneEmail(usuario) {
