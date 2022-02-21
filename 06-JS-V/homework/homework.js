e2aec35a33be4e20bcf027c2216742d397164f60
Roxana <rocevedo00@gmail.com>
2022-02-20 22:22:34 -0300

    last

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -37,10 +37,10 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  string.prototype.reverse = function () {
+  String.prototype.reverse = function () {
     var stringInvertida = "";
     for (var i=this.length - 1; i>=0; i--) {
-      stringInvertida = stringInvertida + this.chatAt(i);
+      stringInvertida = stringInvertida + this.charAt(i);
     }
     return stringInvertida;
   };
@@ -88,7 +88,7 @@ function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
   Persona.prototype.datos = function () {
-    return this.nombre + ", " + this.edad + "años"; 
+    return this.nombre + ", " + this.edad + " años"; 
   }
 }
   
