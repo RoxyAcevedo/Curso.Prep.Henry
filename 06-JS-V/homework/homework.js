4c1a21737fa5d5f046d26c23f7d1b76d13b43a41
Roxana <rocevedo00@gmail.com>
2022-02-20 22:18:43 -0300

    last

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,6 +1,5 @@
 // No cambies los nombres de las funciones.
 
-const { stringify } = require("git-url-parse");
 
 function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
