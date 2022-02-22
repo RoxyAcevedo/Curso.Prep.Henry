8790c9f819601c888b1c0985effcf08ef852f893
Roxana <rocevedo00@gmail.com>
2022-02-22 16:15:15 -0300

    ok

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,27 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  mayuscula = function (nombre) {
-    if (nombre.charAt(0).toUpperCase())
-    return nombre;
-  }
+  return nombre[0].toUpperCase() + nombre.slice
 }
-
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-  invocarCallback (cb); {
-    return cb;
-  }
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  opMat = function(argumento, cb) {
-    return cb (n1, n2);}
+  return cb (n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -32,18 +25,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  var sumarArray = [numeros];
-  var sumaReduce = numeros.reduce(function( acc, elemento) {})
+  var sumaReduce = numeros.reduce(function(acc, curr) {
+    return acc + curr;
+  },0);
+  cb(sumaReduce);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  const array = [];
-  array.forEach(function (cb) {
-    return elemento; 
-  }) 
+  for (var i=0; i < array.length; i++) {
+    cb(array[i]); 
+  }
 }
 
 function map(array, cb) {
@@ -51,8 +45,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código: 
-  var array = []; 
-  var nuevoArray = array.map()
+  var nuevoArray = array.map(function(el) {
+    return cb(el);
+  });
+  return nuevoArray;
 }
 
 function filter(array) {
